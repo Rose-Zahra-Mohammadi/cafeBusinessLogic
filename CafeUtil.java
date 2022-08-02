9cda456be77d0eafda979e46d1f9e871804b3186
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-08-02 15:37:26 -0400

    Initial

---
+++ CafeUtil.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class CafeUtil {
+
+  public int getStreakGoal(int numWeeks) {
+    int sum = 0;
+    for (int i = 1; i <= numWeeks; i++) {
+      sum = sum + i;
+    }
+    return (sum);
+  }
+
+  public double getOrderTotal(double[] prices) {
+    double totalPrice = 0;
+    for (double price : prices) {
+      totalPrice = totalPrice + price;
+    }
+    return (totalPrice);
+  }
+
+  public void displayMenu(ArrayList<String> menuItems) {
+    for (int i = 0; i < menuItems.size(); i++) {
+      String item = menuItems.get(i);
+      System.out.println(i + " " + item);
+    }
+  }
+
+  public void addCustomer(ArrayList<String> customers) {
+    System.out.println("Please enter your name:");
+    String userName = System.console().readLine();
+    System.out.println("Hello" + " " + userName + "!");
+    System.out.println(
+      "There are " + customers.size() + " people in front of you"
+    );
+    customers.add(userName);
+    System.out.println(customers);
+  }
+}
